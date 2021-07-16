b05318677cd50fbc2ee8667b9b07afaaa4e0fb39
haChangJin <hac198912@gmail.com>
2021-07-17 03:50:03 +0900

    - main페이지 추가 - 헤더, 푸터, 컨텐츠(글목록) 영역 추가

---
+++ src/main/resources/static/js/board/main.js
@@ -0,0 +1,11 @@
+
+console.log("board메인 js");
+console.log("board메인 js :: ", boardList);
+
+document.querySelector('#brSaveBtn').addEventListener('click', () => {
+
+});
+
+function insertBoard(){
+    let url = "/board/";
+}
