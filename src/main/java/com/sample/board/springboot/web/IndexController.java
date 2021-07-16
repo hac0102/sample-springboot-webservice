307989f155796a3e4676a51b32212fb909c76022
haChangJin <hac198912@gmail.com>
2021-07-17 03:45:08 +0900

    - url "/" 기본페이지 요청시 main.html 리다이렉트 설ㅈ어

--- src/main/java/com/sample/board/springboot/web/IndexController.java
+++ src/main/java/com/sample/board/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.sample.board.springboot.web;
 
+import com.sample.board.springboot.service.board.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +10,12 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 public class IndexController {
 
+    private final BoardService boardService;
+
     @GetMapping("/")
     public ModelAndView main() throws Exception {
         ModelAndView mv = new ModelAndView();
+        mv.addObject("boardDataList", boardService.getBoardList());
         mv.setViewName("main");
         return mv;
     }
