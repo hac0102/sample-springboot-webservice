6e12eef2621fb022ef7ce2b35b23bb0d9f0eaeb4
haChangJin <hac198912@gmail.com>
2021-07-17 22:29:24 +0900

    - 게시글 상페이지 화면추가 모달

--- src/main/resources/static/js/board/main.js
+++ src/main/resources/static/js/board/main.js
@@ -6,6 +6,13 @@ let board = {
             _this.insertBoard();
         });
 
+        document.querySelector("#boardTable").addEventListener('click', (e) => {
+            let selBrNo = e.target.closest("tr").getAttribute("selBrNo");
+            if(selBrNo == null) return null;
+            console.log(_this);
+            _this.getBoardDetailData(selBrNo);
+        })
+
     },
 
     insertBoard : function() {
@@ -14,10 +21,35 @@ let board = {
         sendData["title"] = document.querySelector('#brTitle').value;
         sendData["content"] = document.querySelector('#brContent').value;
         dataSendPost(url, sendData, "POST");
+    },
+
+    getBoardDetailData : function(selBrNo) {
+        let url = '/board/'
+        getDetailData(url, selBrNo)
+
     }
 };
 
 board.init();
+
+async function getDetailData(url, brNo){
+    fetch(url + (brNo == (undefined || null) ? '' : brNo))
+    .then((res) => {
+        if(res.status != 200) return null;
+//        console.log("res :: ", res);
+//        console.log("res1111 :: ", res.html());
+        return res.text();
+    })
+    .then((data) => {
+        console.log("data ::: ", data);
+        document.querySelector("#testDiv").innerHTML = data;
+    })
+    .catch(err => {
+        console.log("err :: ", err);
+    })
+}
+
+
 
 
 async function dataSendPost(url, data, method) {
