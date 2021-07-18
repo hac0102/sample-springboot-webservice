3ff6a5a193bb3ef5b809884f1b32b2337f6ea56e
haChangJin <hac198912@gmail.com>
2021-07-19 03:16:14 +0900

    - 게시글 수정, 삭제, 등록시   로그인사용자가 등록하게 변경   (기존 테스트로인해 임의사용자정보 등록)

--- src/main/java/com/sample/board/springboot/service/board/BoardService.java
+++ src/main/java/com/sample/board/springboot/service/board/BoardService.java
@@ -1,7 +1,8 @@
 package com.sample.board.springboot.service.board;
 
+import com.sample.board.springboot.config.auth.LoginUser;
+import com.sample.board.springboot.config.auth.dto.SessionUser;
 import com.sample.board.springboot.mapper.board.BoardMapper;
-import com.sample.board.springboot.web.dto.board.Board;
 import com.sample.board.springboot.web.dto.board.BoardListResponseDto;
 import com.sample.board.springboot.web.dto.board.BoardRequestdDto;
 import lombok.RequiredArgsConstructor;
@@ -22,7 +23,6 @@ public class BoardService {
 
     private final BoardMapper boardMapper;
 
-
     public Object getBoardList() {
         //일단 그냥 단순 리스트
         // 나중에 로그인까지되면 내가쓴글or전체글
@@ -30,22 +30,20 @@ public class BoardService {
                 .stream()
                 .filter(list -> "N".equals(list.getDelYn()))
                 .collect(Collectors.toList());
-        log.info("boardListResponseDto :: ", boardListResponseDto);
         return boardListResponseDto;
     }
 
     @Transactional
-    public ResponseEntity<?> insertBoard(BoardRequestdDto boardRequestdDto) {
+    public ResponseEntity<?> insertBoard(BoardRequestdDto boardRequestdDto, SessionUser userInfo) {
         boardRequestdDto = BoardRequestdDto.builder()
                 .title(boardRequestdDto.getTitle())
                 .content(boardRequestdDto.getContent())
                 .delYn("N")
-                .userNo(1)
+                .userNo(userInfo.getUserNo())
                 .build();
 
         int affectRow = boardMapper.insertBoard(boardRequestdDto);
         boardMapper.insertBoardHistory(boardRequestdDto);
-
         return affectRow > 0 ? new ResponseEntity<>(HttpStatus.OK) : new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 
     }
@@ -56,34 +54,32 @@ public class BoardService {
     }
 
     @Transactional
-    public ResponseEntity<?> updateBoard(BoardRequestdDto boardRequestdDto) {
+    public ResponseEntity<?> updateBoard(BoardRequestdDto boardRequestdDto, SessionUser userInfo) {
         boardRequestdDto = BoardRequestdDto.builder()
                 .brNo(boardRequestdDto.getBrNo())
                 .title(boardRequestdDto.getTitle())
                 .content(boardRequestdDto.getContent())
                 .delYn("N")
-                .userNo(1)
+                .userNo(userInfo.getUserNo())
                 .build();
 
         int affectRow = boardMapper.updateBoard(boardRequestdDto);
         boardMapper.insertBoardHistory(boardRequestdDto);
-
         return affectRow > 0 ? new ResponseEntity<>(HttpStatus.OK) : new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
     @Transactional
-    public ResponseEntity<?> deleteBoard(BoardRequestdDto boardRequestdDto) {
+    public ResponseEntity<?> deleteBoard(BoardRequestdDto boardRequestdDto, SessionUser userInfo) {
         boardRequestdDto = BoardRequestdDto.builder()
                 .brNo(boardRequestdDto.getBrNo())
                 .title(boardRequestdDto.getTitle())
                 .content(boardRequestdDto.getContent())
                 .delYn("Y")
-                .userNo(1)
+                .userNo(userInfo.getUserNo())
                 .build();
 
         int affectRow = boardMapper.deleteBoard(boardRequestdDto);
         boardMapper.insertBoardHistory(boardRequestdDto);
-
         return affectRow > 0 ? new ResponseEntity<>(HttpStatus.OK) : new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/sample/board/springboot/web/board/BoardController.java
+++ src/main/java/com/sample/board/springboot/web/board/BoardController.java
@@ -1,7 +1,8 @@
 package com.sample.board.springboot.web.board;
 
+import com.sample.board.springboot.config.auth.LoginUser;
+import com.sample.board.springboot.config.auth.dto.SessionUser;
 import com.sample.board.springboot.service.board.BoardService;
-import com.sample.board.springboot.web.dto.board.Board;
 import com.sample.board.springboot.web.dto.board.BoardRequestdDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,24 +25,23 @@ public class BoardController {
     }
 
     @PostMapping("/api/v1/board")
-    public ResponseEntity<?> insertBoard(@RequestBody BoardRequestdDto boardRequestdDto) {
-        return boardService.insertBoard(boardRequestdDto);
+    public ResponseEntity<?> insertBoard(@RequestBody BoardRequestdDto boardRequestdDto, @LoginUser SessionUser userInfo) {
+        return boardService.insertBoard(boardRequestdDto, userInfo);
     }
 
     @GetMapping("/api/v1/board/{brNo}")
     public ModelAndView getBoadrdDetailData(@PathVariable int brNo) {
-        log.info("brNobrNo :: {}", brNo);
         return boardService.getBoardDetailData(brNo);
     }
 
     @PutMapping("/api/v1/board")
-    public ResponseEntity<?> updateBoard(@RequestBody BoardRequestdDto boardRequestdDto) {
-        return boardService.updateBoard(boardRequestdDto);
+    public ResponseEntity<?> updateBoard(@RequestBody BoardRequestdDto boardRequestdDto, @LoginUser SessionUser userInfo) {
+        return boardService.updateBoard(boardRequestdDto, userInfo);
     }
 
     @DeleteMapping("/api/v1/board")
-    public ResponseEntity<?> deleteBoard(@RequestBody BoardRequestdDto boardRequestdDto) {
-        return boardService.deleteBoard(boardRequestdDto);
+    public ResponseEntity<?> deleteBoard(@RequestBody BoardRequestdDto boardRequestdDto, @LoginUser SessionUser userInfo) {
+        return boardService.deleteBoard(boardRequestdDto, userInfo);
     }
 
 }
